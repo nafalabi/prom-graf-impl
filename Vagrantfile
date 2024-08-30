5dd08f723cbc1ab61dc4b46a91ee4e7c28b0da04
Nanda Abi Fahmi <nafal.abi@gmail.com>
2024-08-30 15:28:39 +0700

    feat: update client setup script

--- Vagrantfile
+++ Vagrantfile
@@ -8,9 +8,12 @@ Vagrant.configure("2") do |config|
     master.vm.box = "hashicorp/bionic64"
     master.vm.box_version = "1.0.282"
     master.vm.network "private_network", ip: "192.168.56.2"
-    master.vm.provision :shell, inline: "sh /vagrant/retrieve-docker.sh"
-    master.vm.provision :shell, inline: "sh /vagrant/install-docker.sh"
-    master.vm.provision :shell, inline: "docker compose -f /vagrant/master/docker-compose.yml up -d"
+    master.vm.provision :shell, inline: <<-SCRIPT
+      sh /vagrant/retrieve-docker.sh
+      sh /vagrant/install-docker.sh
+      /vagrant/client/setup.sh 192.168.56.2
+      docker compose -f /vagrant/master/docker-compose.yml up -d
+    SCRIPT
   end
   
   config.vm.define "client1" do |client1|
@@ -18,8 +21,10 @@ Vagrant.configure("2") do |config|
     client1.vm.box = "hashicorp/bionic64"
     client1.vm.box_version = "1.0.282"
     client1.vm.network "private_network", ip: "192.168.56.3"
-    client1.vm.provision :shell, inline: "sh /vagrant/install-docker.sh"
-    client1.vm.provision :shell, inline: "docker compose -f /vagrant/client/docker-compose.yml up -d"
+    client1.vm.provision :shell, inline: <<-SCRIPT
+      sh /vagrant/install-docker.sh
+      docker compose -f /vagrant/client/docker-compose.yml up -d
+    SCRIPT
   end
   
   config.vm.define "client2" do |client2|
@@ -27,8 +32,10 @@ Vagrant.configure("2") do |config|
     client2.vm.box = "hashicorp/bionic64"
     client2.vm.box_version = "1.0.282"
     client2.vm.network "private_network", ip: "192.168.56.4"
-    client2.vm.provision :shell, inline: "sh /vagrant/install-docker.sh"
-    client2.vm.provision :shell, inline: "docker compose -f /vagrant/client/docker-compose.yml up -d"
+    client2.vm.provision :shell, inline: <<-SCRIPT
+      sh /vagrant/install-docker.sh
+      docker compose -f /vagrant/client/docker-compose.yml up -d
+    SCRIPT
   end
 
 end
--- client/setup.sh
+++ client/setup.sh
@@ -1,56 +1,56 @@
 #!/bin/bash
 
+CLIENT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ARG_PROM_IP="$1"
+
 # Function to read input with a prompt
 read_input() {
-    local prompt="$1"
-    local input_var="$2"
-    read -p "$prompt: " "$input_var"
+  local prompt="$1"
+  local input_var="$2"
+  read -p "$prompt: " "$input_var"
 }
 
 # Function to validate an IP address
 validate_ip() {
-    ip="$1"
-    echo "$ip" | grep -E '^([0-9]{1,3}\.){3}[0-9]{1,3}$' > /dev/null
-    if [ $? -eq 0 ]; then
-        return 0
-    else
-        echo "Invalid IP address. Please enter a valid IP address."
-        return 1
-    fi
+  ip="$1"
+  echo "$ip" | grep -E '^([0-9]{1,3}\.){3}[0-9]{1,3}$' >/dev/null
+  if [ $? -eq 0 ]; then
+    return 0
+  else
+    echo "Invalid IP address. Please enter a valid IP address."
+    return 1
+  fi
 }
 
 # Function to validate a domain name
 validate_domain() {
-    domain="$1"
-    echo "$domain" | grep -E '^[a-zA-Z0-9.-]+$' > /dev/null
-    if [ $? -eq 0 ]; then
-        return 0
-    else
-        echo "Invalid domain name. Please enter a valid domain name."
-        return 1
-    fi
+  domain="$1"
+  echo "$domain" | grep -E '^[a-zA-Z0-9.-]+$' >/dev/null
+  if [ $? -eq 0 ]; then
+    return 0
+  else
+    echo "Invalid domain name. Please enter a valid domain name."
+    return 1
+  fi
 }
 
 echo "Setting up client config"
 
-cp nginx.conf_sample nginx.conf
+cp $CLIENT_DIR/nginx.conf_sample $CLIENT_DIR/nginx.conf
 
-# Get whitelisted IP address
-while true; do
+WHITELIST_IP="$ARG_PROM_IP"
+
+if [[ -z $WHITELIST_IP ]]; then
+  # Get whitelisted IP address
+  while true; do
     read_input "Enter Prometheus IP" whitelist_ip
     if validate_ip "$whitelist_ip"; then
-        sed -i 's/__PROMETHEUS_IP__/'"$whitelist_ip"'/g' nginx.conf
-        break
+      WHITELIST_IP="$whitelist_ip"
+      break
     fi
-done
+  done
+fi
 
-# Get current domain name
-while true; do
-    read_input "Enter domain name" domain_name
-    if validate_domain "$domain_name"; then
-        sed -i 's/__DOMAIN_NAME__/'"$domain_name"'/g' nginx.conf
-        break
-    fi
-done
+sed -i 's/__PROMETHEUS_IP__/'"$WHITELIST_IP"'/g' $CLIENT_DIR/nginx.conf
 
 echo "Setup complete!"
